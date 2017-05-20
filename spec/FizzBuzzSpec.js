9a2facddfd89347a7738ad8d11f1cae13529e9cc
Greg Orton <gorton@Gregs-MBP.lan>
2017-05-19 20:13:48 -0500

    clearer test names

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -6,12 +6,12 @@ describe("FizzBuzz", function() {
         fizzbuzz = new FizzBuzz();
     });
 
-    it("should return 1", function() {
+    it("should return 1 when 1 is passed", function() {
         var result = fizzbuzz.fizzbuzz(1);
         expect(1).toEqual(result);
     });
 
-    it("should return fizz", function() {
+    it("should return fizz when 3 is passed", function() {
         var result = fizzbuzz.fizzbuzz(3);
         expect('fizz').toEqual(result);
     });
